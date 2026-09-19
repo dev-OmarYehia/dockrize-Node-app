ca25fc6a01a2c2bb2f751973bbb98d2c3f837f52
Omar Yehia <omaryehia138@gmail.com>
2026-09-19 15:52:36 +0300

    bulid form zero

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-const fs = require('fs');
-const folderpath = "D:\\firstapp";
-fs.readdir(folderpath , (err,files)=>{
-    console.log("files : " + files);
-    files.forEach((FileName , index)=>{
-        console.log("FileName : " + FileName);
-    })``
-});
+const http = require('http');
