b17faafa6f5fa762e5130aeebefdeecf2e6428c5
Omar Yehia <omaryehia138@gmail.com>
2026-09-19 16:02:43 +0300

    Basic Informational Site

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    fs.readFile('index.html', (err, data) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Internal Server Error');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+      }
+    });
+  } else if (req.url === '/about') {
+    fs.readFile('about.html', (err, data) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Internal Server Error');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+      }
+    });
+  }
+});
+
+server.listen(8080, () => {
+  console.log('Server is running on http://localhost:8080>');
+});
+    
